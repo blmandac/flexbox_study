406ffad7af0bae25bc410bb4e6670c112317b85e
Bien Justin Mandac <blmandac@localadmins-MacBook-Pro.local>
2015-09-04 15:26:51 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,27 @@
 module.exports = function(grunt) {
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         compass: {
+            dist: {
+                options: {
+                    sassDir: 'assets/sass',
+                    cssDir: 'assets/css',
+                    noLineComments: true
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'assets/css/style.min.css' : ['assets/css/style.css']
+                }
+            }
         }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['compass']);
 };
